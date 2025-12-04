4ea01f3d69024f10f33b1ad5c5e5e68a4e2d8ad3
Charly <charlyplanquettepro@gmail.com>
2025-12-04 14:43:06 +0100

    change module and replace whisper

--- app/utils/whisper_engine.py
+++ app/utils/whisper_engine.py
@@ -1,14 +1,23 @@
-import whisper
+from openai import OpenAI
+client = OpenAI()
 
-model = whisper.load_model("base")
-
-def transcribe_audio(audio_path):
-    result = model.transcribe(audio_path)
-    return result["text"]
-
+def transcribe_audio(audio_path: str) -> str:
+    with open(audio_path, "rb") as audio_file:
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file
+        )
+    return transcript.text
 # def transcribe_audio(file_path: str) -> str:
 #     """
 #     Transcrit un fichier audio en texte avec Whisper.
 #     """
 #     result = model.transcribe(file_path)
 #     return result.get("text", "")
+
+
+# model = whisper.load_model("base")
+
+# def transcribe_audio(audio_path):
+#     result = model.transcribe(audio_path)
+#     return result["text"]
