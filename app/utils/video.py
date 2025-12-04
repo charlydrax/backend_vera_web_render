2a4b4d6ad3628cd714c3c3261146f683d1585a8e
Charly <charlyplanquettepro@gmail.com>
2025-12-04 13:54:45 +0100

    add ffmpeg

--- app/utils/video.py
+++ app/utils/video.py
@@ -1,4 +1,4 @@
-# import ffmpeg
+import ffmpeg
 import os
 import subprocess
 from .whisper_engine import transcribe_audio
@@ -7,13 +7,13 @@ def video_to_text(video_path):
     audio_path = "/tmp/audio.wav"
 
     # Extraire audio
-    # (
-    #     ffmpeg
-    #     .input(video_path)
-    #     .output(audio_path, ac=1, ar="16000")
-    #     .overwrite_output()
-    #     .run()
-    # )
+    (
+        ffmpeg
+        .input(video_path)
+        .output(audio_path, ac=1, ar="16000")
+        .overwrite_output()
+        .run()
+    )
 
     # Transcrire audio
     text = transcribe_audio(audio_path)
