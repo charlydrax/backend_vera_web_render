e10770fde9927fbba81a1f6bd973750e4e59671e
Charly <charlyplanquettepro@gmail.com>
2025-12-04 14:18:55 +0100

    add whisper module

--- app/utils/whisper_engine.py
+++ app/utils/whisper_engine.py
@@ -1,15 +1,14 @@
-# import whisper
+import whisper
 
-# model = whisper.load_model("base")
+model = whisper.load_model("base")
 
-# def transcribe_audio(audio_path):
-#     result = model.transcribe(audio_path)
-#     return result["text"]
+def transcribe_audio(audio_path):
+    result = model.transcribe(audio_path)
+    return result["text"]
 
-def transcribe_audio(file_path: str) -> str:
-    """
-    Transcrit un fichier audio en texte avec Whisper.
-    """
-    # result = model.transcribe(file_path)
-    # return result.get("text", "")
-    return " Indisponible "
+# def transcribe_audio(file_path: str) -> str:
+#     """
+#     Transcrit un fichier audio en texte avec Whisper.
+#     """
+#     result = model.transcribe(file_path)
+#     return result.get("text", "")
